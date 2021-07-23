d98fe63f8d519cf18b978d82573a36a6abf73912
micson-us <micson@deloitte.com>
2021-07-23 12:13:41 -0400

    fixed kp name

--- terraform/elrr_key_pair.tf
+++ terraform/elrr_key_pair.tf
@@ -1,9 +1,9 @@
 resource "aws_key_pair" "elrr_public_kp" {
   key_name = "elrr_public_kp"
-  public_key = file("key.pub")
+  public_key = file("elrr_public_kp.pub")
   }
 
 resource "aws_key_pair" "elrr_private_kp" {
   key_name = "elrr_private_kp"
-  public_key = file("key.pub")
+  public_key = file("elrr_private_kp.pub")
   }
